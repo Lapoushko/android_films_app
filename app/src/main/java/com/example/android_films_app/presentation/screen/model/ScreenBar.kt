7b78884683ad90b44ed6f02383a25891c98ffd99
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-17 01:22:20 +0500

    bottom bar and navigation added

--- app/src/main/java/com/example/android_films_app/presentation/screen/model/ScreenBar.kt
+++ app/src/main/java/com/example/android_films_app/presentation/screen/model/ScreenBar.kt
@@ -1,4 +1,4 @@
-package com.example.android_films_app.presentation.screen
+package com.example.android_films_app.presentation.screen.model
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -10,67 +10,52 @@ import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.example.android_films_app.presentation.model.FilmItem
 
 /**
  * @author Lapoushko
  *
- * Экран
+ * Экраны нижнего бара
  */
-sealed class Screen(
+sealed class ScreenBar(
     val route: String,
     val title: String? = null,
     val setIcon: ImageVector? = null,
     val unsetIcon: ImageVector? = null
-) {
+){
     /**
      * Экран фильмов
      */
-    data object Films : Screen(
+    data object Films : ScreenBar(
         route = FILMS_ROUTE,
         title = FILMS_TITLE,
         setIcon = FILMS_SET_ICON,
         unsetIcon = FILMS_UNSET_ICON
     )
-    data object FilmDetails : Screen(
-        route = "${FILMS_DETAILS_ROUTE}/{film}",
-        title = FILMS_DETAILS_TITLE,
-        setIcon = FILMS_DETAILS_SET_ICON,
-        unsetIcon = FILMS_DETAILS_UNSET_ICON
-    ) {
-        fun createRoute(film: FilmItem?) = "${FILMS_DETAILS_ROUTE}/${film}"
-    }
-    data object Home : Screen(
+
+    data object Home : ScreenBar(
         route = HOME_ROUTE,
         title = HOME_TITLE,
         setIcon = HOME_SET_ICON,
         unsetIcon = HOME_UNSET_ICON
     )
-    data object Favourite : Screen(
+    data object Favourite : ScreenBar(
         route = FAVOURITE_ROUTE,
         title = FAVOURITE_TITLE,
         setIcon = FAVOURITE_SET_ICON,
         unsetIcon = FAVOURITE_UNSET_ICON
     )
-    data object Notifications : Screen(
+    data object Notifications : ScreenBar(
         route = NOTIFICATIONS_ROUTE,
         title = NOTIFICATIONS_TITLE,
         setIcon = NOTIFICATIONS_SET_ICON,
         unsetIcon = NOTIFICATIONS_UNSET_ICON
     )
 
-    companion object {
-        private const val FILM_ID_KEY = "FILM_ID"
-
+    companion object{
         private const val FILMS_ROUTE = "FILMS_ROUTE"
         private const val FILMS_TITLE = "Фильмы"
         private val FILMS_SET_ICON = Icons.Filled.AccountCircle
         private val FILMS_UNSET_ICON = Icons.Outlined.AccountCircle
-
-        private const val FILMS_DETAILS_ROUTE = "FILMS_DETAILS_ROUTE"
-        private const val FILMS_DETAILS_TITLE = "Подробнее о фильме"
-        private val FILMS_DETAILS_SET_ICON = Icons.Filled.AccountCircle
-        private val FILMS_DETAILS_UNSET_ICON = Icons.Outlined.AccountCircle
 
         private const val HOME_ROUTE = "HOME_ROUTE"
         private const val HOME_TITLE = "Домашняя страница"
