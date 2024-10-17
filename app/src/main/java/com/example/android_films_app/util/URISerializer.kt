668c3da3cece1fec3dada992897c1360ec30e041
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-17 22:25:48 +0500

    For navigation uri serializer

---
+++ app/src/main/java/com/example/android_films_app/util/URISerializer.kt
@@ -0,0 +1,23 @@
+package com.example.android_films_app.util
+
+import android.net.Uri
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+/**
+ * @author Lapoushko
+ */
+object URISerializer : KSerializer<Uri> {
+    override val descriptor = PrimitiveSerialDescriptor("Uri", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Uri {
+        return Uri.parse(decoder.decodeString())
+    }
+
+    override fun serialize(encoder: Encoder, value: Uri) {
+        encoder.encodeString(value.toString())
+    }
+}
