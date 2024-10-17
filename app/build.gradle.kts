5282ba32d7c16d5f4c9127b42be6f798548360a8
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-10-17 22:26:26 +0500

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,9 +72,9 @@ dependencies {
 
     implementation(libs.coil.compose)
 
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
