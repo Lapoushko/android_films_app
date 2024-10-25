2ea4c07b7aff5eb94421d3ca3e4eafb89e8ab1f7
Lapoushko <vadim.dovgiy@mail.ru>
2024-10-25 23:50:05 +0500

    replace repo

---
+++ app/src/main/java/com/example/android_films_app/domain/repository/FilmsRepository.kt
@@ -0,0 +1,23 @@
+package com.example.android_films_app.domain.repository
+
+import com.example.android_films_app.domain.entity.Film
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * @author Lapoushko
+ *
+ * Репозиторий фильмов
+ */
+interface FilmsRepository{
+    /**
+     * Получить все фильмы
+     * @return Flow всех фильмов domain
+     */
+    suspend fun getFilms(): Flow<List<Film>>
+
+//    /**
+//     * Получить фильм domain
+//     * @return Flow фильма domain
+//     */
+//    suspend fun getFilm(id: Long): Flow<Film>
+}
