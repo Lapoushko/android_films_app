11ee2daf6d576b6dd7c17e7ab737713030d7ba0f
Lapoushko <vadim.dovgiy@mail.ru>
2024-11-18 16:33:00 +0500

    fix badges

--- app/src/main/java/com/example/android_films_app/presentation/viewModel/BottomBarScreenViewModel.kt
+++ app/src/main/java/com/example/android_films_app/presentation/viewModel/BottomBarScreenViewModel.kt
@@ -7,7 +7,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -29,7 +28,9 @@ class BottomBarScreenViewModel @Inject constructor(
 
     fun load(){
         viewModelScope.launch {
-            _countQueries.value = subscribeGetQueriesUseCase.getQueries().first().size
+            subscribeGetQueriesUseCase.getQueries().collect{
+                _countQueries.value = it.size
+            }
         }
     }
 }
